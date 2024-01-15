8a031aa87619ba8aaa8e8913d7ddb3f721ec779a
renatech <samuelserenaluv@gmail.com>
2024-01-15 14:00:30 +0100

    Created app layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return(
+    <div id="main-container">
+      <div id="app-container">
+        
+      </div>
+    </div>
+  )
+};
+
+export default App;
